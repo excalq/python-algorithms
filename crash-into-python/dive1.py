781228b45fea74b9258630b8d1cf8269183aae36
Arthur Kepler <dev@arthurk.com>
2019-10-04 22:13:59 -0700

    Remove extraneous string with XML path

--- crash-into-python/dive1.py
+++ crash-into-python/dive1.py
@@ -3,8 +3,6 @@ import glob
 import humansize
 from pprint import pprint
 
-'/Users/arthur/Hacking/Android/AndroidSDK/samples/android-23/legacy/ApiDemos/res/layout'
-
 dir = '/Users/arthur/Hacking/Android/AndroidSDK/samples/android-23/legacy/ApiDemos/res/layout/'
 path = os.path.join(dir, '*.xml')
 xml = [(humansize.approximate_size(os.stat(f).st_size), f) for f in glob.glob(path)] 
